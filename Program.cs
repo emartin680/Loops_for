09c2c3695ba66fc6928c97adedcf30aaa23adb4c
Jules C White <62078259+juleswhi@users.noreply.github.com>
2023-09-24 23:51:58 +0100

    Syntax Hightling and README

---
+++ Program.cs
@@ -0,0 +1,15 @@
+class Program {
+    public static void Main(string[] args) {
+        // 1. This for loop will print the numbers from 1 to 50 in ascending order
+
+        for(int i = 1; i <= 50; i++) {
+            Console.WriteLine(i);
+        }
+
+        // 2. This for loop will print the numbers from 50 to 1 in descending order
+
+        for(int i = 50; i > 0; i --){
+            Console.WriteLine(i);
+        }
+    }
+}
