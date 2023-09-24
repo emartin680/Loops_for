8b7bc1e593eef43f0b4791c1b674fddc1d5b25a2
Jules C White <62078259+juleswhi@users.noreply.github.com>
2023-09-24 23:53:56 +0100

    Fix formatting issue

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 class Program {
     public static void Main(string[] args) {
+
         // 1. This for loop will print the numbers from 1 to 50 in ascending order
 
         for(int i = 1; i <= 50; i++) {
